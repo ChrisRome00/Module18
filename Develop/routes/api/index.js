dd6e963f39b9f9d2862f0fbdc68a1e1558af35d7
Christopher Romero <romero.chrisprof@gmail.com>
2024-08-26 18:30:50 -0500

    Added boiler plate code

---
+++ Develop/routes/api/index.js
@@ -0,0 +1,7 @@
+const router = require('express').Router();
+//import new api route files
+
+//now it will be localhost:3001/api/extension
+//router.use('/extention', newely imported api routes)
+
+module.exports = router;
